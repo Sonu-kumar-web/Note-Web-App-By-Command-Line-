919b2849b1a92cfeeac901d449e03dc12f2a5768
Sonu <sonu3660@gmail.com>
2020-07-21 00:47:01 +0530

    Add list command

--- app.js
+++ app.js
@@ -8,62 +8,66 @@ yargs.version("1.1.0");
 // Add, Remove, Read and list notes
 // Create add Command
 yargs.command({
-  command: "add",
-  describe: "Add a new notes",
-  builder: {
-    title: {
-      describe: "Note title",
-      demandOption: true,
-      type: "string",
-    },
-    body: {
-      describe: "Note body",
-      demandOption: true,
-      type: "string",
-    },
-  },
-  handler: (argv) => {
-    // console.log("Title: " + argv.title);
-    // console.log("Body: " + argv.body);
+   command: "add",
+   describe: "Add a new notes",
+   builder: {
+      title: {
+         describe: "Note title",
+         demandOption: true,
+         type: "string",
+      },
+      body: {
+         describe: "Note body",
+         demandOption: true,
+         type: "string",
+      },
+   },
+   //  ES6 short hand syntax
+   handler(argv) {
+      // console.log("Title: " + argv.title);
+      // console.log("Body: " + argv.body);
 
-    notes.addNote(argv.title, argv.body);
-
-  },
+      notes.addNote(argv.title, argv.body);
+   },
 });
 
 // Create a remove command
 yargs.command({
-  command: "remove",
-  describe: "Remove a notes",
-  builder: {
-    title: {
-      describe: 'Note title',
-      demandOption: true,
-      type: 'string'
-    }
-  },
-  handler: (argv) => {
-    // console.log("Removing a notes");
-    notes.removeNote(argv.title);
-  },
+   command: "remove",
+   describe: "Remove a notes",
+   builder: {
+      title: {
+         describe: "Note title",
+         demandOption: true,
+         type: "string",
+      },
+   },
+   //  ES6 short hand syntax
+   handler(argv) {
+      // console.log("Removing a notes");
+      notes.removeNote(argv.title);
+   },
 });
 
 // Create a list Command
 yargs.command({
-  command: "list",
-  describe: "listing your notes",
-  handler: () => {
-    console.log("Listing all notes");
-  },
+   command: "list",
+   describe: "listing your notes",
+   //  ES6 short hand syntax
+   handler() {
+      // console.log("Listing all notes");
+      notes.listNotes();
+   },
 });
 
 // Create read Command
 yargs.command({
-  command: "read",
-  describe: "Reading a notes",
-  handler: () => {
-    console.log("reading a notes");
-  },
+   command: "read",
+   describe: "Reading a notes",
+   //  ES6 short hand syntax
+   handler() {
+      console.log("reading a notes");
+   },
 });
 
 // console.log(yargs.argv);
--- notes.js
+++ notes.js
@@ -1,79 +1,79 @@
-const fs = require('fs');
-const chalk = require('chalk');
+const fs = require("fs");
+const chalk = require("chalk");
 
-const getNotes = () => 'Your Notes....';
+const getNotes = () => "Your Notes....";
 
 // For add notes
 const addNote = (title, body) => {
+   const notes = loadNotes();
+   // console.log(notes);
 
-    const notes = loadNotes();
-    // console.log(notes);
+   // prevent from add more than one title in an note
+   //    const duplicateNotes = notes.filter((note) => {
+   //       return note.title === title;
+   //    });
 
-    // prevent from add more than one title in an note
-    const duplicateNotes = notes.filter((note) => {
-        return note.title === title;
-    });
+   const duplicateNotes = notes.find((note) => {
+      return note.title === title;
+   });
 
-
-    if (duplicateNotes.length === 0) {
-
-        notes.push({
-            title: title,
-            body: body
-        });
-        // console.log(notes);
-        saveNotes(notes);
-        console.log(chalk.green.inverse('New note Added!'));
-    } else {
-        console.log(chalk.red.inverse('Note title taken!'));
-
-    }
-
-}
+   if (!duplicateNotes) {
+      notes.push({
+         title: title,
+         body: body,
+      });
+      // console.log(notes);
+      saveNotes(notes);
+      console.log(chalk.green.inverse("New note Added!"));
+   } else {
+      console.log(chalk.red.inverse("Note title taken!"));
+   }
+};
 
 // To Remove a note
 const removeNote = (title) => {
+   const notes = loadNotes();
+   const notesToKeep = notes.filter((note) => {
+      return note.title !== title; // if its return true it will add in noteToKeep array automatically if false the it will not add.
+   });
 
-    const notes = loadNotes();
-    const notesToKeep = notes.filter((note) => {
-        return note.title !== title; // if its return true it will add in noteToKeep array automatically if false the it will not add.
-    });
+   if (notes.length > notesToKeep.length) {
+      console.log(chalk.green.inverse("Note Removed!"));
+      saveNotes(notesToKeep);
+   } else {
+      console.log(chalk.red.inverse("No note found!"));
+   }
+};
 
-    if (notes.length > notesToKeep.length) {
-        console.log(chalk.green.inverse('Note Removed!'));
-        saveNotes(notesToKeep);
-
-    } else {
-        console.log(chalk.red.inverse('No note found!'));
-
-    }
-
+// To get list of notes
+const listNotes = () => {
+   const notes = loadNotes();
+   console.log(chalk.yellow.inverse("Your notes"));
+   notes.forEach((note) => {
+      console.log(note.title);
+   });
 };
 
 // Save data into file
 const saveNotes = (notes) => {
-
-    const dataJSON = JSON.stringify(notes);
-    fs.writeFileSync('notes.json', dataJSON);
-
+   const dataJSON = JSON.stringify(notes);
+   fs.writeFileSync("notes.json", dataJSON);
 };
 
-// Load notes from file 
+// Load notes from file
 const loadNotes = () => {
-
-    try {
-        const dataBuffer = fs.readFileSync('notes.json');
-        const dataJSON = dataBuffer.toString();
-        return JSON.parse(dataJSON);
-
-    } catch (error) {
-        return [];
-    }
+   try {
+      const dataBuffer = fs.readFileSync("notes.json");
+      const dataJSON = dataBuffer.toString();
+      return JSON.parse(dataJSON);
+   } catch (error) {
+      return [];
+   }
 };
-
 
 module.exports = {
-    getNotes: getNotes,
-    addNote: addNote,
-    removeNote: removeNote
-}
+   getNotes: getNotes,
+   addNote: addNote,
+   removeNote: removeNote,
+   listNotes: listNotes,
+};
